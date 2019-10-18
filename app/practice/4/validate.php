772edcc3034b61f028b2770b7f221ad5e65023da
shoutakeshita2019 <united.Uldaha@outlook.jp>
2019-10-18 21:23:52 +0900

    ４章終了

--- app/practice/4/validate.php
+++ app/practice/4/validate.php
@@ -1,5 +1,16 @@
 <?php
 //ポスト内容を取得し、入力値が正しいか検証する。
+$movie = '';
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $movie = $_POST['movie'];
+
+    if(mb_strlen($movie) === 0){
+    $err = '文字を入力してください。';
+    }elseif(mb_strlen($movie) > 20){
+    $err = '２０文字以内で入力してください。';
+    }
+}
 ?>
 
 <html>
@@ -15,6 +26,11 @@ input{margin:5px;}
         <p>
             <?php
             //入力内容に誤りがあればエラーメッセージを、正しければ「あなたの好きな映画は〜です」と表示する
+            if(isset($err)){
+                echo $err;
+            }else{
+                echo 'あなたの好きな映画は'.$movie.'です。';
+            }
             ?>
         </p>
         <form action="" method="POST">
